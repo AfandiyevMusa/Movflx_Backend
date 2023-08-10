da8cf05a43f86a7400c960ed55a349de074475c5
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-08-10 09:53:38 -0400

    frontto back also ready

---
+++ Final_Project/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Final_Project.Services.Interfaces
+{
+	public interface IEmailService
+	{
+        //void Send(string to, string subject, string html, string from = null);
+<<<<<<< HEAD
+=======
+        void Send(string to, string subject, string html, string from = null);
+
+>>>>>>> 4036f43 (login register are ready)
+    }
+}
+
---
+++ Final_Project/wwwroot/admin/forms/Readme.txt
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+Fully working PHP/AJAX contact form script is available in the pro version of the template.
+=======
+Fully working PHP/AJAX contact form script is available in the pro version of the template.
+>>>>>>> 4036f43 (login register are ready)
+You can buy it from: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
